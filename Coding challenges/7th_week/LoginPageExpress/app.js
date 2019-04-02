5654270ee6a316d414d152b0d96d871b5ac65549
abhishekmalviya17 <47751567+abhishekmalviya17@users.noreply.github.com>
2019-04-02 10:44:53 +0530

    login

--- Coding challenges/7th_week/LoginPageExpress/app.js
+++ Coding challenges/7th_week/LoginPageExpress/app.js
@@ -8,7 +8,12 @@ app.set("body-parser","hbs");
 
 app.use(parser.urlencoded({extended:false}));
 
-var loginDB=[];
+
+var userDB = [
+    {name: "ansal@gmail.com", password: "ansal123"},
+    {name: "vaibhav", password: "vaibhav123"},
+    {name: "divyam", password: "divyam123"}
+  ];
 
 app.get("/login",function(req,res){
 
@@ -18,18 +23,15 @@ res.render("index.hbs")
 
 app.post("/login",function(req,res){
     var DB={};
-    DB.username=req.body.EmailID;
-    DB.password=req.body.Password;
+    username=req.body.EmailID;
+    password=req.body.Password;
 
-    var userDB = [
-        {name: "ansal@gmail.com", password: "ansal123"},
-        {name: "vaibhav", password: "vaibhav123"},
-        {name: "divyam", password: "divyam123"}
-      ];
+   
 
-      for(var i=0;i<=userDB.length;i++){
-          if(DB.username==userDB.name[i]&&DB.Password==userDB.password[i]){
+      for(var i=0;i<userDB.length;i++){
+          if(username==userDB.name[i]&&password==userDB.password[i]){
               alert("Logged in");
+              res.redirect("/login")
               
           }
           else{
