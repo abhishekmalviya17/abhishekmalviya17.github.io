e8fe649cf0266d7fb8b73c06015c6c8095934334
abhishekmalviya17 <abhishekmalviya17@gmail.com>
2019-05-01 10:02:15 +0530

    update

--- Coding challenges/10th week/SquaresByFilter/script.js
+++ Coding challenges/10th week/SquaresByFilter/script.js
@@ -13,7 +13,7 @@ console.log(sqArray);
 squareOfNumber([11,22,45,32,12,52,68,23,14,61,10]);
 
 function squareByFunctional(num){
-var sqArray=num.filter(function(number){if{return number%2==0}}).map(function(number){return number*number});
+var sqArray=num.filter(function(number){return number%2==0}).map(function(number){return number*number});
 console.log(sqArray);
 }
 
